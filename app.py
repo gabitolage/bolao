ed148442094a3b7ae245da78663f94c7ea0fdeaa
gabitolage <gglage11@gmail.com>
2026-05-08 18:00:29 -0300

    fix: ajustar margin do botao para 50vw

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ else:
                     cursor: pointer;
                     width: auto;
                     max-width: 90%;
-                    margin: 0 10vw;
+                    margin: 0 50vw;
                     display: block;
                 }
 
